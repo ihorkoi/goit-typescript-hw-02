9a8bb6e14f73ce46b59c00fafad23d570fbed0d4
ihorkoi <koleev.igor@gmail.com>
2023-10-26 09:41:50 +0300

    solved

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,15 +1,15 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
-interface T {
+interface ComponentProps {
   title: string;
 }
 
-class Component {
+class Component<T extends ComponentProps> {
   constructor(public props: T) {}
 }
 
-class Page extends Component {
+class Page extends Component<ComponentProps> {
   pageInfo() {
     console.log(this.props.title);
   }
--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,8 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key: NonNullable<unknown>;
-  value: any;
+interface KeyValuePair<K, V> {
+  key: K;
+  value: V;
 }
-
 export {};
